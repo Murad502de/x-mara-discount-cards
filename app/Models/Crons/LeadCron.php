8a9d736afcd3d87fa6ef7feed210e803aea3bc3a
itWelt <rassulovmurad@gmail.com>
2022-09-13 11:46:15 +0300

    [main][task] deploy

--- app/Models/Crons/LeadCron.php
+++ app/Models/Crons/LeadCron.php
@@ -55,13 +55,15 @@ class LeadCron extends Model
             ->get();
 
         foreach ($leads as $lead) {
-            // $LEAD = Lead::getLeadByAmoId((int) $lead->lead_id);
+            $LEAD = Lead::getLeadByAmoId((int) $lead->lead_id);
 
-            // $LEAD ? self::haveAvailabilityLead($lead, $LEAD) : self::dontHaveAvailabilityLead($lead);
+            $LEAD ? self::haveAvailabilityLead($lead, $LEAD) : self::dontHaveAvailabilityLead($lead);
 
             Log::info(__METHOD__, ['Scheduler::[LeadCron][parseRecentWebhooks][DELETE] ' . $lead->lead_id]); //DELETE
 
             $lead->delete();
+
+            Log::info(__METHOD__, ['Scheduler::[LeadCron][parseRecentWebhooks][DELETEd]']); //DELETE
         }
     }
 
